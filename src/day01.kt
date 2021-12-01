8e4b2501affe3dd711b96c3db6289634d66f5c40
Francis Duvivier <francisduvivier@gmail.com>
2021-12-01 23:02:07 +0100

    Add day01 part1 solution

---
+++ src/day01.kt
@@ -0,0 +1,23 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        var count = 0;
+        for (i in 1..input.size - 1) {
+            if (input[i].toInt() > input[i - 1].toInt()) {
+                count++
+            }
+        }
+        return count;
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("day01.test")
+    check(part1(testInput) == 7)
+
+    val input = readInput("day01")
+    prcp(part1(input))
+    prcp(part2(input))
+}
