97b67c9c2438ef3812b12ebf7ab92c6a872cba57
Francis Duvivier <francisduvivier@gmail.com>
2021-12-02 07:17:08 +0100

    Solve day02 part2

--- src/day02.kt
+++ src/day02.kt
@@ -23,12 +23,35 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        var depth = 0;
+        var hor = 0;
+        var aim = 0;
+        for (instuct in input) {
+            val splitInstruct = instuct.split(" ")
+            val opcode = splitInstruct[0]
+            val amount = splitInstruct[1].toInt()
+            if (opcode.equals("up")) {
+                aim -= amount;
+                if (depth < 0) {
+                    throw Error("submarines can't fly")
+                }
+            } else if (opcode.equals("down")) {
+                aim += amount;
+            } else if (opcode.equals("forward")) {
+                hor += amount;
+                depth += aim * amount
+            } else {
+                throw Error("Invalid opcode")
+            }
+        }
+        return depth * hor;
     }
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("day02.test")
     check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
 
     val input = readInput("day02")
     prcp(part1(input))
