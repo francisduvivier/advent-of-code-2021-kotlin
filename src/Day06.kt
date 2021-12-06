c396e17c171f27a90c42e74f602bada4840002f1
Francis Duvivier <francisduvivier@gmail.com>
2021-12-06 07:54:02 +0100

    First try day06 part 1

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,6 +1,24 @@
 fun main() {
+    fun iterateOnce(lanterns: MutableList<Int>) {
+        for (lanternI in 0..lanterns.size - 1) {
+            lanterns[lanternI]--;
+            if (lanterns[lanternI] == -1) {
+                lanterns[lanternI] = 6
+                lanterns.add(8)
+            }
+        }
+    }
+
+    fun iterateLife(startLanterns: MutableList<Int>, cycles: Int) {
+        for (i in 0..cycles - 1) {
+            iterateOnce(startLanterns)
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val lanternTtls = input[0].split(",").map { it.toInt() }.toMutableList()
+        iterateLife(lanternTtls, 80)
+        return lanternTtls.size
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +27,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06.test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 5934)
 
     val input = readInput("Day06")
     prcp(part1(input))
