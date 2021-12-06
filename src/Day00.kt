5f838b76b073c140bdd3785a2d987a281d5c4d14
Francis Duvivier <francisduvivier@gmail.com>
2021-12-06 07:40:15 +0100

    Rename day_ to day00

--- src/Day00.kt
+++ src/Day00.kt
@@ -8,10 +8,10 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day_.test")
+    val testInput = readInput("Day00.test")
     check(part1(testInput) == 1)
 
-    val input = readInput("Day_")
+    val input = readInput("Day00")
     prcp(part1(input))
     prcp(part2(input))
 }
