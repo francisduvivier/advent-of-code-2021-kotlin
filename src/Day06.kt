33119885ac0df977c492d7c78cae13f07a8c9238
Francis Duvivier <francisduvivier@gmail.com>
2021-12-06 07:41:46 +0100

    Start day06

---
+++ src/Day06.kt
@@ -0,0 +1,17 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day06.test")
+    check(part1(testInput) == 1)
+
+    val input = readInput("Day06")
+    prcp(part1(input))
+    prcp(part2(input))
+}
