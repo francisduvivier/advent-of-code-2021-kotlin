273d5bacd580f93be3917d94ef6fd7078a95096f
Francis Duvivier <francisduvivier@gmail.com>
2021-12-10 07:55:06 +0100

    Try Solve Day10 Part2

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val scoreMap = mapOf(
+    val syntaxCheckerScoreMap = mapOf(
         ')' to 3,
         ']' to 57,
         '}' to 1197,
@@ -21,7 +21,7 @@ fun main() {
                     if (stack.last() == matchMap[char]) {
                         stack.removeLast()
                     } else {
-                        score += scoreMap[char]!!
+                        score += syntaxCheckerScoreMap[char]!!
                         break;
                     }
                 } else {
@@ -32,15 +32,51 @@ fun main() {
         return score
     }
 
+    val autoCompleteScoreMap = mapOf(
+        '(' to 1,
+        '[' to 2,
+        '{' to 3,
+        '<' to 4
+    )
+
     fun part2(input: List<String>): Int {
-        return 0
+        val scores = ArrayList<Int>()
+        for (line in input) {
+            val stack = ArrayList<Char>()
+            var corrupt = false;
+            for (char in line) {
+                if (matchMap.containsKey(char)) {
+                    if (stack.last() == matchMap[char]) {
+                        stack.removeLast()
+                    } else {
+                        corrupt = true
+                        break;
+                    }
+                } else {
+                    stack.add(char)
+                }
+            }
+            if (!corrupt) {
+                if (stack.isEmpty()) {
+                    throw Error("empty stack")
+                }
+                val score =
+                    stack.reversed().map { autoCompleteScoreMap[it]!! }
+                        .reduce { acc, value -> acc * 5 + value }
+                scores.add(score)
+            }
+        }
+        val middleIndex = scores.size / 2
+        return scores.sorted()[middleIndex]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day10.test")
     check(part1(testInput) == 26397)
+    check(part2(testInput) == 288957)
 
     val input = readInput("Day10")
     prcp(part1(input))
     prcp(part2(input))
 }
+
