64f26225f38c9a13c4b810ea26124f71b3cd8433
Francis Duvivier <francisduvivier@gmail.com>
2021-12-11 15:49:01 +0100

    Solve Day 11 part 2

--- src/Day11.kt
+++ src/Day11.kt
@@ -36,12 +36,31 @@ fun main() {
     }
 
     fun part2(input: List<String>): Long {
-        return 0
+        val board = input.map { line -> line.toCharArray().map { it.toString().toInt() }.toIntArray() }.toTypedArray()
+        val rowCols = rowCols(board)
+        val nbStebs = 10000
+        for (step in 1..nbStebs) {
+            for ((row, col) in rowCols) {
+                board[row][col]++
+            }
+            do {
+                val prevVal = totalFlashes
+                for ((row, col) in rowCols) {
+                    increase8Neighbors(board, row, col)
+                }
+            } while (prevVal != totalFlashes)
+            if (board.map { it.sum() }.sum() == 0) {
+                return step.toLong()
+            }
+        }
+        totalFlashes = 0;
+        throw Error("did not find sync point")
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day11.test")
     check(part1(testInput) == 1656.toLong())
+    check(part2(testInput) == 195.toLong())
 
     val input = readInput("Day11")
     prcp(part1(input))
