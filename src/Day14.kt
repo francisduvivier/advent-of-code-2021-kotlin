4ba02b241e62d7ade53f764d5d96df7d48526318
Francis Duvivier <francisduvivier@gmail.com>
2021-12-14 07:41:43 +0100

    Solve Day14 part 1

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,6 +1,38 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun doReplacements(
+        nbReplacements: Int,
+        replacements: List<List<String>>,
+        currentString: String
+    ): String {
+        var resultString: String = currentString
+        for (i in 0 until nbReplacements) {
+            for ((match, insert) in replacements) {
+                do {
+                    val startLength = resultString.length
+                    resultString = resultString.replace(match, "${match[0]}_${insert}_${match[1]}")
+                } while (resultString.length > startLength)
+            }
+            resultString = resultString.replace("_", "");
+        }
+        return resultString
+    }
+
+    fun toCharCountMap(resultString: String): Map<Char, Long> {
+        val charMap = HashMap<Char, Long>()
+        for (char in resultString) {
+            charMap[char] = (charMap[char] ?: 0) + 1
+        }
+        return charMap
+    }
+
+    fun part1(input: List<String>): Long {
+        var currentString = input[0]
+        val replacements = input.subList(2, input.size).map { it.split(" -> ") }
+        val nbReplacements = 10
+        val resultString = doReplacements(nbReplacements, replacements, currentString)
+        val charCounts = toCharCountMap(resultString)
+        val sortedCounts = charCounts.values.sorted()
+        return -sortedCounts.first() + sortedCounts.last()
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +41,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day14.test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 1588.toLong())
 
     val input = readInput("Day14")
     prcp(part1(input))
