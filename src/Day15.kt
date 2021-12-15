2560bedffd6a712853b8acdaaa8088d56335ba4b
Francis Duvivier <francisduvivier@gmail.com>
2021-12-15 07:25:57 +0100

    Use better method than recDepth for limiting recursion depth

--- src/Day15.kt
+++ src/Day15.kt
@@ -9,13 +9,15 @@ fun main() {
         col: Int,
         recDepth: Int = 0
     ) {
-        if (recDepth > bestCostMatrix.size * 5 || bestCostMatrix.size <= row || bestCostMatrix[0].size <= col) {
+        if (bestCostMatrix.size <= row || bestCostMatrix[0].size <= col) {
             return;
         }
         val currCost = bestCostMatrix[row][col]
         for ((otherRow, otherCol) in getNeighborLocations(bestCostMatrix, row, col)) {
             val newCost = currCost + getCost(otherRow, otherCol, row, col)
-            if (bestCostMatrix[otherRow][otherCol] > newCost) {
+            if (bestCostMatrix[otherRow][otherCol] > newCost && bestCostMatrix.last()
+                    .last() > newCost + bestCostMatrix.size - 1 - row - bestCostMatrix[0].size - 1 - col
+            ) {
                 bestCostMatrix[otherRow][otherCol] = newCost
                 findBestPathRec(bestCostMatrix, getCost, otherRow, otherCol, recDepth + 1)
             }
@@ -27,6 +29,10 @@ fun main() {
         val bestCostMatrix = matrix.map { it.map { Long.MAX_VALUE }.toMutableList() }
         val getCost = { toRow: Int, toCol: Int, fromRow: Int, fromCol: Int -> matrix[toRow][toCol] }
         bestCostMatrix[0][0] = 0.toLong()
+        bestCostMatrix.last()[bestCostMatrix.lastIndex] = Math.min(
+            matrix[0].map { it.toLong() }.sum() + matrix.map { it[it.lastIndex].toLong() }.sum(),
+            matrix[matrix.lastIndex].map { it.toLong() }.sum() + matrix.map { it[0].toLong() }.sum()
+        ) - matrix[0][0]// Add minimum cost to solution to prevent stackoverflow for very long paths
         findBestPathRec(bestCostMatrix, getCost, 0, 0)
         return bestCostMatrix.last().last()
     }
