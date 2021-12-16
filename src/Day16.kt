bb00a0c7f997376964350481779b7cf6dc1f9839
Francis Duvivier <francisduvivier@gmail.com>
2021-12-16 08:53:32 +0100

    Prepare Day16 Tests

--- src/Day16.kt
+++ src/Day16.kt
@@ -9,7 +9,11 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day16.test")
-    checkEquals(part1(testInput), 1)
+
+    checkEquals(part1(listOf("8A004A801A8002F478")), 16)
+    checkEquals(part1(listOf("620080001611562C8802118E34")), 12)
+    checkEquals(part1(listOf("C0015000016115A2E0802F182340")), 23)
+    checkEquals(part1(listOf("A0016C880162017C3686B18A3D4780")), 31)
 
     val input = readInput("Day16")
     prcp(part1(input))
