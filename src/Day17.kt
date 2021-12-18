19da912f22979fcd24bac03ffbcb05e26a6fd029
Francis Duvivier <francisduvivier@gmail.com>
2021-12-18 21:47:52 +0100

    Solve and simplify Day 17

--- src/Day17.kt
+++ src/Day17.kt
@@ -5,75 +5,43 @@ fun main() {
         return maxVal * (maxVal + 1) / 2
     }
 
-    fun findXVelocityForRangeSteps(range: IntRange, steps: Int): Int? {
-        for (velocity in 0..range.last) {
-            val endPosition = velocity * Math.min(steps, velocity) - calcIncreasingSum(Math.min(steps, velocity) - 1)
-            if (range.contains(endPosition)) {
-                return velocity
-            }
-        }
-        return null
-    }
 
-    fun part1(x: IntRange, y: IntRange): Int {
-
-        for (currVelYTry in -y.first - 1..-y.first - 1) {
-            println("currVelYTry: $currVelYTry")
-            for (currEndYTry in y) {
-                val nbSteps = 2 * currVelYTry + 1
-                val matchingX = findXVelocityForRangeSteps(x, nbSteps)
-                if (matchingX != null) {
-                    return calcIncreasingSum(currVelYTry)
+    fun findAllMatchingVelocities(
+        xRange: IntRange,
+        yRange: IntRange
+    ): HashMap<String, Pair<Int, Int>> {
+        val allMatches = HashMap<String, Pair<Int, Int>>()
+        for (testX in 0..xRange.last) {
+            for (testY in yRange.first..-yRange.first) {
+                var currXVel = testX
+                var currYVel = testY
+                var currXPos = 0
+                var currYPos = 0
+                for (steps in 1..3 * -yRange.first) {
+                    currXPos += currXVel
+                    currYPos += currYVel
+                    if (currXVel > 0) {
+                        currXVel -= 1
+                    }
+                    currYVel -= 1
+                    if (currXPos in xRange && currYPos in yRange) {
+                        allMatches.set("${testX},${testY}", Pair(testX, testY))
+                        continue
+                    } else if (currXPos > xRange.last || currYPos < yRange.first) {
+                        continue
+                    }
                 }
             }
         }
-        return -1
+        return allMatches
     }
 
-    fun findYMatchesForRangeSteps(range: IntRange, steps: Int): Set<Int> {
-        var matches = HashSet<Int>()
-        var negativeSteps = steps
-        for (velocity in range.first..-range.first) {
-            var absVel = abs(velocity)
-            if (velocity >= 0) {
-                negativeSteps -= 2 * absVel + 1
-            }
-            if (negativeSteps < 0) {
-                continue
-            }
-            var endPosition = -(absVel * negativeSteps + calcIncreasingSum(negativeSteps - 1))
-
-            if (range.contains(endPosition)) {
-                matches.add(velocity)
-            }
-        }
-        return matches
+    fun part1(x: IntRange, y: IntRange): Int {
+        return findAllMatchingVelocities(x, y).values.map { calcIncreasingSum(it.second) }.maxOrNull()!!
     }
 
-    fun findXMatchesForRangeSteps(range: IntRange, steps: Int): Set<Int> {
-        var matches = HashSet<Int>()
-        for (velocity in 0..range.last * range.last + 1) {
-            val endPosition = velocity * Math.min(steps, velocity) - calcIncreasingSum(Math.min(steps, velocity) - 1)
-            if (range.contains(endPosition)) {
-                matches.add(endPosition);
-            }
-        }
-        return matches
-    }
-
-    fun part2(x: IntRange, y: IntRange): Int {
-        val allMatches = HashSet<String>()
-        for (nbSteps in 1..100 * -y.first) {
-            val matchingYs = findYMatchesForRangeSteps(y, nbSteps)
-            if (matchingYs.isEmpty()) {
-                continue
-            }
-            val matchingXs = findXMatchesForRangeSteps(x, nbSteps)
-            if (matchingXs.isEmpty()) {
-                continue
-            }
-            allMatches.addAll(matchingYs.map { y -> matchingXs.map { x -> "x${x}y${y}" } }.flatten())
-        }
+    fun part2(xRange: IntRange, yRange: IntRange): Int {
+        val allMatches = findAllMatchingVelocities(xRange, yRange)
         return allMatches.size
     }
 
@@ -84,6 +52,6 @@ fun main() {
     val xRange = 257..286
     val yRange = -101..-57
     prcp(part1(x = xRange, y = yRange))
-//    checkEquals(part2(x = xTestRange, y = yTestRange), 112)
-    prcp(part2(x = xRange, y = yRange))
+    checkEquals(part2(xRange = xTestRange, yRange = yTestRange), 112)
+    prcp(part2(xRange = xRange, yRange = yRange))
 }
