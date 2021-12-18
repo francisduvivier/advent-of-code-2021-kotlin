68668ee5e1f326d14e5af7129ee236395582570f
Francis Duvivier <francisduvivier@gmail.com>
2021-12-18 06:56:57 +0100

    WIP Day17 Part2

--- src/Day17.kt
+++ src/Day17.kt
@@ -1,9 +1,11 @@
+import kotlin.math.abs
+
 fun main() {
     fun calcIncreasingSum(maxVal: Int): Int {
         return maxVal * (maxVal + 1) / 2
     }
 
-    fun findVelocityForRangeSteps(range: IntRange, steps: Int): Int? {
+    fun findXVelocityForRangeSteps(range: IntRange, steps: Int): Int? {
         for (velocity in 0..range.last) {
             val endPosition = velocity * Math.min(steps, velocity) - calcIncreasingSum(Math.min(steps, velocity) - 1)
             if (range.contains(endPosition)) {
@@ -19,7 +21,7 @@ fun main() {
             println("currVelYTry: $currVelYTry")
             for (currEndYTry in y) {
                 val nbSteps = 2 * currVelYTry + 1
-                val matchingX = findVelocityForRangeSteps(x, nbSteps)
+                val matchingX = findXVelocityForRangeSteps(x, nbSteps)
                 if (matchingX != null) {
                     return calcIncreasingSum(currVelYTry)
                 }
@@ -28,8 +30,51 @@ fun main() {
         return -1
     }
 
+    fun findYMatchesForRangeSteps(range: IntRange, steps: Int): Set<Int> {
+        var matches = HashSet<Int>()
+        var negativeSteps = steps
+        for (velocity in range.first..-range.first) {
+            var absVel = abs(velocity)
+            if (velocity >= 0) {
+                negativeSteps -= 2 * absVel + 1
+            }
+            if (negativeSteps < 0) {
+                continue
+            }
+            var endPosition = -(absVel * negativeSteps + calcIncreasingSum(negativeSteps - 1))
+
+            if (range.contains(endPosition)) {
+                matches.add(velocity)
+            }
+        }
+        return matches
+    }
+
+    fun findXMatchesForRangeSteps(range: IntRange, steps: Int): Set<Int> {
+        var matches = HashSet<Int>()
+        for (velocity in 0..range.last * range.last + 1) {
+            val endPosition = velocity * Math.min(steps, velocity) - calcIncreasingSum(Math.min(steps, velocity) - 1)
+            if (range.contains(endPosition)) {
+                matches.add(endPosition);
+            }
+        }
+        return matches
+    }
+
     fun part2(x: IntRange, y: IntRange): Int {
-        return 0
+        val allMatches = HashSet<String>()
+        for (nbSteps in 1..100 * -y.first) {
+            val matchingYs = findYMatchesForRangeSteps(y, nbSteps)
+            if (matchingYs.isEmpty()) {
+                continue
+            }
+            val matchingXs = findXMatchesForRangeSteps(x, nbSteps)
+            if (matchingXs.isEmpty()) {
+                continue
+            }
+            allMatches.addAll(matchingYs.map { y -> matchingXs.map { x -> "x${x}y${y}" } }.flatten())
+        }
+        return allMatches.size
     }
 
     // test if implementation meets criteria from the description, like:
@@ -39,6 +84,6 @@ fun main() {
     val xRange = 257..286
     val yRange = -101..-57
     prcp(part1(x = xRange, y = yRange))
-    checkEquals(part2(x = xTestRange, y = yTestRange), 0)
+//    checkEquals(part2(x = xTestRange, y = yTestRange), 112)
     prcp(part2(x = xRange, y = yRange))
 }
