11ef10b597a83446f47f9d04e0122e4580e0e517
Francis Duvivier <francisduvivier@gmail.com>
2021-12-28 17:50:03 +0200

    Prepare part2

--- src/Day16.kt
+++ src/Day16.kt
@@ -92,28 +92,42 @@ fun main() {
         return sum + children.map { addVersionNumberRec(it) }.sum()
     }
 
-    fun part1(input: List<String>): Int {
-        val inputBits = input[0].fromHexToBits()
+    fun part1(input: String): Int {
+        val inputBits = input.fromHexToBits()
         val topPacket = parsePacketRec(BITStream(inputBits))
         return addVersionNumberRec(topPacket)
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: String): Long {
         return 0
     }
 
+    fun part2(input: List<String>): Long {
+        return part2(input[0])
+    }
     // test if implementation meets criteria from the description, like:
     val literalPacketBitsTest = "110100101111111000101000"
     val literalTestHex = "D2FE28"
     checkEquals(literalTestHex.fromHexToBits(), literalPacketBitsTest)
     checkEquals(parseLiteral(BITStream(literalPacketBitsTest.substring(6))), 2021)
-    checkEquals(part1(listOf(literalTestHex)), 6)
-    checkEquals(part1(listOf("8A004A801A8002F478")), 16)
-    checkEquals(part1(listOf("620080001611562C8802118E34")), 12)
-    checkEquals(part1(listOf("C0015000016115A2E0802F182340")), 23)
-    checkEquals(part1(listOf("A0016C880162017C3686B18A3D4780")), 31)
+    checkEquals(part1((literalTestHex)), 6)
+    checkEquals(part1("8A004A801A8002F478"), 16)
+    checkEquals(part1("620080001611562C8802118E34"), 12)
+    checkEquals(part1("C0015000016115A2E0802F182340"), 23)
+    checkEquals(part1("A0016C880162017C3686B18A3D4780"), 31)
 
     val input = readInput("Day16")
-    prcp(part1(input))
-    prcp(part2(input))
+    prcp(part1(input[0]))
+    checkEquals(part2("04005AC33890"), 54)
+    checkEquals(part2("C200B40A82"), 3)
+    checkEquals(part2("04005AC33890"), 4)
+    checkEquals(part2("880086C3E88112"), 7)
+    checkEquals(part2("CE00C43D881120"), 9)
+    checkEquals(part2("D8005AC2A8F0"), 5)
+    checkEquals(part2("F600BC2D8F"), 5)
+    checkEquals(part2("9C005AC2F8F0"), 5)
+    checkEquals(part2("9C0141080250320F1802104A08"), 1)
+    prcp(part2(input[0]))
 }
+
+
